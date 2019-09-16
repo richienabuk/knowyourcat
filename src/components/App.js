c1abf7af0c936ba98c54ee7aeef43a9bd72d9cc9
Imo-owo Nabuk <richienabuk@gmail.com>
2019-09-16 18:41:04 +0100

    ch(setup): Complete eslint config update

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const App = () => {
-    return (
-        <div>
-            <div className="container">
-                <h1>KnowYourCat App</h1>
-                <p>
-                    Setup completed
-                </p>
-            </div>
-        </div>
-    );
-};
+const App = () => (
+  <div>
+    <div className="container">
+      <h1>KnowYourCat App</h1>
+      <p>
+        Setup completed
+      </p>
+    </div>
+  </div>
+);
 
 export default App;
